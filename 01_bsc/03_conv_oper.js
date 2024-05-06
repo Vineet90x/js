c5d0afaf76541faa8acd7fbae8a1bfbcc8d5d661
Vineet90x <78544085+Vineet90x@users.noreply.github.com>
2024-05-06 02:11:58 +0000

    operations

---
+++ 01_bsc/03_conv_oper.js
@@ -0,0 +1,62 @@
+let score = null
+console.log(typeof score);
+
+let valueinnumber = Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber)
+
+// "33" = 33
+// "33abc"= nan
+// true = 1 / false = 0
+
+let isloggin = 3
+let bool = Boolean(isloggin)
+console.log(bool)
+
+// 1 = ture, 0 = false
+// "" = false
+// "hitesh" = true
+
+
+let num= 33
+let str1 = String(num)
+console.log(typeof str1)
+
+
+//operations
+
+let value = 3
+nvalue = -value
+console.log(nvalue)
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2); 
+console.log(2%2); 
+
+let str3 = "hello"
+let str4 = " vineet"
+let str5 = str3+str4
+console.log(str5);
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log((3+4)*5%3)
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
