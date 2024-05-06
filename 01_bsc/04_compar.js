1820c52072af604a230ead21b836346c5c0716da
Vineet90x <78544085+Vineet90x@users.noreply.github.com>
2024-05-06 02:37:45 +0000

    comparison

---
+++ 01_bsc/04_compar.js
@@ -0,0 +1,30 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2" > 1)
+console.log("2" < 1)
+
+console.log("null")
+console.log(null > 0)
+console.log(null < 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null <= 0)
+
+
+console.log("undefined")
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+
+
+// === strict check
+console.log("===");
+console.log("2" === 2);
+
+
