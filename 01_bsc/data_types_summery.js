92ceb9a727eb289f7734001b940493b96c3e4da3
Vineet90x <78544085+Vineet90x@users.noreply.github.com>
2024-05-09 02:34:07 +0000

    stack and heap

--- 01_bsc/data_types_summery.js
+++ 01_bsc/data_types_summery.js
@@ -37,3 +37,29 @@ myfunction();
 
 console.log(typeof userEmail)
 
+
+
+
+// memory 
+
+// stack(primitive) and heap(non-primitive)
+
+let myyoutubename = "alpha"
+let anothernaem = myyoutubename
+anothernaem = "lmao"
+
+console.log(anothernaem);
+console.log(myyoutubename);
+
+let user1 = {
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let user2 = user1
+
+user2.email = "alpha@google.com"
+
+console.log(user1.email);
+console.log(user2.email);
+console.log(user1);
